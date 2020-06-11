896f56c583a462fb30043e62a74cd4a43c3505b7
Roy <roy.rojval@gmail.com>
2020-06-11 13:23:54 -0500

    add figure component

---
+++ src/components/figureComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-if="number">
+            <div v-if="number>0">
+                <div v-for="index in number" :key="index" style="display: inline-block;">
+                    <img :src="iconSvg" style="width: 50px;" />
+                </div>
+            </div>
+            <div v-if="number<0" style="display: flex;align-items: center;">
+                <img :src="'/assets/stop.svg'" style="width: 50px;" />
+                <div style="display: inline-block;width: 210px;margin:10px;">
+                    <small>{{errormsg}}</small>
+                </div>
+            </div>
+        </div>
+        <div v-if="!number"><img :src="'/assets/zero.svg'" style="width: 50px;" />
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'figureComponent',
+        props: {
+            number: {
+                type: Number
+            },
+            errormsg: {
+                type: String,
+                default: 'Lo sentimos, no puedes quitarte más de lo que tenias originalmente'
+            },
+            iconSvg: {
+                type: String,
+                default: '/assets/apple.svg'
+            }
+        }
+    }
+</script>
--- src/views/themes/addition.vue
+++ src/views/themes/addition.vue
@@ -4,13 +4,15 @@
             <h2>Sumas</h2>
         </div>
         <div class="section">
-
+            <FigureComponent v-bind:number="4" style="display: inline-block;margin:5px 0px" />
         </div>
     </div>
 </template>
 <script>
+    import FigureComponent from '../../components/figureComponent.vue';
     export default {
         name: 'Addition',
+        components: { FigureComponent },
         data() {
             return {
                 numbers: [2, 4]
