d6aaae98b4a1cb58834d4d63eee5db0b34c1657b
Roy <roy.rojval@gmail.com>
2020-06-10 23:37:20 -0500

    fix nav bar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,16 +1,16 @@
 <template id="navBar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" v-bind:class=" { 'navbarOpen': show }">
-        <button class="navbar-toggler" type="button" v-b-toggle.sidebar-backdrop>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" v-bind:class=" { 'navbarOpen': true }">
+        <button id="btnToogleHamburger" class="navbar-toggler" type="button" v-b-toggle.sidebar-backdrop>
             <span class="navbar-toggler-icon"></span>
         </button>
-        <b-sidebar id="sidebar-backdrop" title="Sammy Learning" :backdrop-variant="variant" backdrop shadow>
+        <b-sidebar id="sidebar-backdrop" title="Sammy Learning" backdrop shadow>
             <template v-slot:default="{ hide }">
                 <div class="p-3">
                     <nav class="mb-3">
                         <b-nav vertical>
-                            <b-nav-item active @click="hide">Active</b-nav-item>
-                            <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
-                            <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+                            <b-nav-item v-for="course in courses" :key="course.CourseId"
+                                @click="selectTheme(course.CourseId);hide()">{{course.Title}}
+                            </b-nav-item>
                         </b-nav>
                     </nav>
                 </div>
@@ -19,21 +19,30 @@
     </nav>
 </template>
 <script>
+    import { courseService } from '../services';
+
     export default {
         name: 'navBar',
         data() {
             return {
                 show: true,
+                courses: [],
             }
         },
+        created() {
+            this.courses = courseService.getCourses();
+            console.log(this.courses);
+        },
         methods: {
-
+            selectTheme(courseId) {
+                console.log("selectTheme", courseId);
+            }
         },
     }
 </script>
 
 <style scoped>
-    .menu-item {
-        padding: 5px;
+    #btnToogleHamburger {
+        display: block;
     }
 </style>
---
+++ src/services/course.service.js
@@ -0,0 +1,16 @@
+const courses = [
+    { CourseId: 1, Title: "Matemática", Icon: 'math.svg' },
+    { CourseId: 2, Title: "Aritmetica", Icon: 'arithmetic.svg' },
+    { CourseId: 3, Title: "Algebra", Icon: 'algebra.svg' },
+    { CourseId: 4, Title: "Trigonometria", Icon: 'trigonometry.svg' },
+    { CourseId: 5, Title: "Geometria", Icon: 'geometry.svg' },
+    { CourseId: 6, Title: "Fisica", Icon: 'physics.svg' },
+];
+
+const getCourses = function () {
+    return courses;
+};
+
+export const courseService = {
+    getCourses,
+}
