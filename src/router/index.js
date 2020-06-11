81241d0cff3e312afe3947d8f1dd2f5e0989d16e
Roy <roy.rojval@gmail.com>
2020-06-11 11:26:38 -0500

    adding page not found

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Index from '../views/Index.vue'
+import PageNotFound from '../views/PageNotFound.vue';
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    component: PageNotFound,
   }
 ]
 
