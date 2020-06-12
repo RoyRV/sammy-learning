9a9a2dedea8d8c9483a825b8da398d77f6b36ea8
Roy <roy.rojval@gmail.com>
2020-06-12 12:02:27 -0500

    commit division

--- src/components/figureComponent.vue
+++ src/components/figureComponent.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        <div v-if="number">
-            <div v-if="number>0">
-                <div v-for="index in number" :key="index" style="display: inline-block;">
-                    <img :src="iconSvg" style="width: 50px;" />
+    <b-container>
+        <b-row>
+            <b-col>
+                <div v-if="number">
+                    <div v-if="number>0">
+                        <div v-for="index in number" :key="index" style="display: inline-block;">
+                            <img :src="iconSvg" style="width: 50px;" />
+                        </div>
+                    </div>
+                    <div v-if="number<0" style="display: inline-block;align-items: center;">
+                        <img :src="'/assets/stop.svg'" style="width: 50px;" />
+                        <div style="display: inline-block;margin:10px;">
+                            <small>{{errormsg}}</small>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div v-if="number<0" style="display: flex;align-items: center;">
-                <img :src="'/assets/stop.svg'" style="width: 50px;" />
-                <div style="display: inline-block;width: 210px;margin:10px;">
-                    <small>{{errormsg}}</small>
+                <div v-if="!number"><img :src="'/assets/zero.svg'" style="width: 50px;" />
                 </div>
-            </div>
-        </div>
-        <div v-if="!number"><img :src="'/assets/zero.svg'" style="width: 50px;" />
-        </div>
-    </div>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 <script>
     export default {
--- src/views/themes/division.vue
+++ src/views/themes/division.vue
@@ -5,15 +5,109 @@
                 <h2>División</h2>
             </b-col>
         </b-row>
+        <b-row>
+            <b-col>
+                <b-container>
+                    <b-row>
+                        <b-col>
+                            <vue-slider v-model="numbers[0]" :max="50" />
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col>
+                            <span v-if="numbers[0]">{{numbers[0]}}</span>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-col>
+            <b-col>
+                <div>
+                    <img :src="'/assets/division.svg'" style="width: 60px;" />
+                </div>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-container>
+                    <b-row>
+                        <b-col>
+                            <vue-slider v-model="numbers[1]" :max="50" />
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col>
+                            <span v-if="numbers[1]">{{numbers[1]}}</span>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-col>
+            <b-col>
+                <div>
+                    <img :src="'/assets/equal.svg'" style="width: 60px;" />
+                </div>
+            </b-col>
+        </b-row>
+        <hr />
+        <b-row>
+            <b-col>
+                <b-container>
+                    <b-row v-if="numbers[1]==0">
+                        <b-col>
+                            <FigureComponent v-bind:number="-1" errormsg='Operación no permitida' />
+                        </b-col>
+                    </b-row>
+                    <b-row v-if="numbers[0]==0 && numbers[1]!=0">
+                        <b-col>
+                            <FigureComponent v-bind:number="0" />
+                        </b-col>
+                    </b-row>
+                    <b-row v-if="numbers[0]!=0 && numbers[1]!=0">
+                        <b-col>
+                            <div v-if="numbers[0]<numbers[1]">
+                                <FigureComponent v-bind:number="0" />
+                            </div>
+                            <div v-if="numbers[0]>=numbers[1]">
+                                <h4>Cociente</h4>
+                                <div style="display: inline-block;" v-for="(number,index) in numbers[1]" :key="number">
+                                    <span>Grupo {{index+1}}</span>
+                                    <FigureComponent v-bind:number="quotient" />
+                                </div>
+                            </div>
+                        </b-col>
+                        <b-col v-if="residue!=0">
+                            <div>
+                                <h4>Residuo</h4>
+                                <FigureComponent v-bind:number="residue" />
+                            </div>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-col>
+        </b-row>
     </b-container>
 </template>
 <script>
+    import FigureComponent from '../../components/figureComponent.vue';
     export default {
         name: 'Division',
+        components: { FigureComponent },
         data() {
             return {
                 numbers: [6, 3]
             }
         },
+        computed: {
+            quotient() {
+                return Math.floor(this.numbers[0] / this.numbers[1]);
+            },
+            residue() {
+                return (this.numbers[0] % this.numbers[1]);
+            }
+        },
     }
 </script>
+<style scoped>
+    hr {
+        border-top-width: 10px;
+    }
+</style>
