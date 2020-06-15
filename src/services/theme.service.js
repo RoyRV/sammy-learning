88e89a76540f13b5fdbabb7b3d30066a03370583
Roy <roy.rojval@gmail.com>
2020-06-15 12:54:03 -0500

    adding square

--- src/services/theme.service.js
+++ src/services/theme.service.js
@@ -3,13 +3,14 @@ const themes = [
     { CourseId: 1, ThemeId: 2, ThemeTitle: 'Resta', redirectTo: 'substraction' },
     { CourseId: 1, ThemeId: 3, ThemeTitle: 'Multiplicación', redirectTo: 'multiplication' },
     { CourseId: 1, ThemeId: 4, ThemeTitle: 'División', redirectTo: 'division' },
-    { CourseId: 1, ThemeId: 11, ThemeTitle: 'Mayor/Menor', redirectTo: 'greaterOrLess' },
+    { CourseId: 1, ThemeId: 5, ThemeTitle: 'Mayor/Menor', redirectTo: 'greaterOrLess' },
     { CourseId: 1, ThemeId: 6, ThemeTitle: 'Exponenciación', redirectTo: 'exponentiation' },
     { CourseId: 1, ThemeId: 7, ThemeTitle: 'Raices', redirectTo: 'square' },
-    { CourseId: 1, ThemeId: 10, ThemeTitle: 'Porcentaje', redirectTo: 'percentage' },
-    { CourseId: 1, ThemeId: 8, ThemeTitle: 'MCM', redirectTo: 'division' },
-    { CourseId: 1, ThemeId: 9, ThemeTitle: 'MCD', redirectTo: 'division' },
-    { CourseId: 1, ThemeId: 5, ThemeTitle: 'Operaciones Combinadas', redirectTo: 'mixedOperations' },
+    { CourseId: 1, ThemeId: 8, ThemeTitle: 'Porcentaje', redirectTo: 'percentage' },
+    { CourseId: 1, ThemeId: 9, ThemeTitle: 'MCM', redirectTo: 'division' },
+    { CourseId: 1, ThemeId: 10, ThemeTitle: 'MCD', redirectTo: 'division' },
+    { CourseId: 1, ThemeId: 11, ThemeTitle: 'Operaciones Combinadas', redirectTo: 'mixedOperations' },
+    { CourseId: 1, ThemeId: 12, ThemeTitle: 'Sistemas Númericos', redirectTo: 'numericSystem' },
 ];
 
 const getThemesByCourseId = function (courseId) {
--- src/views/themes/square.vue
+++ src/views/themes/square.vue
@@ -5,30 +5,46 @@
                 <h2>Raíces</h2>
             </b-col>
         </b-row>
-        <b-row class="mt-4">
-            <b-col cols="12" md="6">
-                <b-row>
-                    <b-col>
-                        <vue-slider v-model="number" :max="10" />
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <span>Base : {{number}}</span>
-                    </b-col>
-                </b-row>
-            </b-col>
-            <b-col cols="12" md="6">
-                <b-row>
-                    <b-col>
-                        <vue-slider v-model="squareFactor" :max="6" />
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <span>Raíz {{squareFactor}}</span>
-                    </b-col>
-                </b-row>
+        <b-row>
+            <b-col>
+                <div role="tablist">
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" role="tab">
+                            <b-button block v-b-toggle.accordion-1 variant="info">Raices Cuadradas</b-button>
+                        </b-card-header>
+                        <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text>
+                                    <b-row>
+                                        <b-col>
+                                            <vue-slider v-model="number" :data="data" :marks="true" />
+                                        </b-col>
+                                    </b-row>
+                                    <b-row class="mt-5">
+                                        <b-col>
+                                            <span>Radicando : {{number}}</span>
+                                        </b-col>
+                                    </b-row>
+                                    <b-row class="mt-4">
+                                        <b-col>
+                                            <span>Se lee: raíz cuadrada(indice 2) de {{number}}</span>
+                                        </b-col>
+                                    </b-row>
+                                    <b-row class="mt-4">
+                                        <b-col>
+                                            <span>Es {{result}}</span>
+                                        </b-col>
+                                    </b-row>
+                                    <b-row class="mt-4">
+                                        <b-col>
+                                            <span>Porque {{result}} x {{result}} = {{number}}</span>
+                                        </b-col>
+                                    </b-row>
+                                </b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
+                </div>
             </b-col>
         </b-row>
     </div>
@@ -38,13 +54,13 @@
         name: 'Square',
         data() {
             return {
-                number: 2,
-                squareFactor: 2
+                number: 0,
+                data: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
             }
         },
         computed: {
-            value() {
-                return 1;
+            result() {
+                return Math.sqrt(this.number, 2)
             }
         }
     }
