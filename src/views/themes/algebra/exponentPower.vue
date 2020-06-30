d65c80cf44db2901babf4611afc1ffed9f3fb8a5
Roy <roy.rojval@gmail.com>
2020-06-30 16:14:27 -0500

    rename vairable

--- src/views/themes/algebra/exponentPower.vue
+++ src/views/themes/algebra/exponentPower.vue
@@ -38,42 +38,146 @@
                                                 <b-col cols="12" md="6">
                                                     <b-row>
                                                         <b-col>
-                                                            <vue-slider v-model="sameBaseMultiExpo1" :max="50" />
+                                                            <vue-slider v-model="sameBaseSumExpo1" :max="50" />
                                                         </b-col>
                                                     </b-row>
                                                     <b-row class="mb-4">
                                                         <b-col>
-                                                            <span>Exponente 1 : {{sameBaseMultiExpo1}}</span>
+                                                            <span>Exponente m : {{sameBaseSumExpo1}}</span>
                                                         </b-col>
                                                     </b-row>
                                                 </b-col>
                                                 <b-col cols="12" md="6">
                                                     <b-row>
                                                         <b-col>
-                                                            <vue-slider v-model="sameBaseMultiExpo2" :max="50" />
+                                                            <vue-slider v-model="sameBaseSumExpo2" :max="50" />
                                                         </b-col>
                                                     </b-row>
                                                     <b-row class="mb-4">
                                                         <b-col>
-                                                            <span>Exponente 2 : {{sameBaseMultiExpo2}}</span>
+                                                            <span>Exponente n : {{sameBaseSumExpo2}}</span>
                                                         </b-col>
                                                     </b-row>
                                                 </b-col>
                                             </b-row>
                                             <b-row>
                                                 <b-col>
-                                                    <span class="description">a</span>
-                                                    <sup class="expo">{{sameBaseMultiExpo1}}</sup>
-                                                    <span> x </span>
-                                                    <span class="description">a</span>
-                                                    <sup class="expo">{{sameBaseMultiExpo2}}</sup>
+                                                    <span>
+                                                        <span class="description">a</span>
+                                                        <sup class="expo">{{sameBaseSumExpo1}}</sup>
+                                                        <span> x </span>
+                                                        <span class="description">a</span>
+                                                        <sup class="expo">{{sameBaseSumExpo2}}</sup>
+
+                                                    </span>
+                                                    <span> = </span>
+                                                    <span>
+                                                        <span class="description">a</span>
+                                                        <sup id="multiExpoSolution" class="expo">{{sameBaseSumExpo1}}
+                                                            + {{sameBaseSumExpo2}}</sup>
+                                                    </span>
+                                                    <b-tooltip target="multiExpoSolution"
+                                                        :title="multiExpoSolutionDesc" />
                                                     <span> = </span>
                                                     <span class="description">a</span>
-                                                    <sup class="expo">{{sameBaseMultiExpo1}} +
-                                                        {{sameBaseMultiExpo2}}</sup>
+                                                    <sup class="expo">{{sameBaseSumExpo1 + sameBaseSumExpo2}}</sup>
+                                                </b-col>
+                                            </b-row>
+                                        </b-col>
+                                    </b-row>
+                                </b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
+
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" role="tab">
+                            <b-button block v-b-toggle.accordion-2 variant="info">División de bases iguales
+                            </b-button>
+                        </b-card-header>
+                        <b-collapse id="accordion-2" visible accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text>
+                                    <b-row>
+                                        <b-col cols="12" md="4" class="mb-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p>Cuando tienen bases iguales, los exponentes se restan</p>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">m</sup>
+                                                    <span> / </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">n</sup>
                                                     <span> = </span>
                                                     <span class="description">a</span>
-                                                    <sup class="expo">{{sameBaseMultiExpo1 + sameBaseMultiExpo2}}</sup>
+                                                    <sup class="expo">m-n</sup>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row class="mt-2">
+                                                <b-col>
+                                                    <b-icon id="divisionExpoRestriction" icon="info-circle" scale="0.75"
+                                                        shift-v="0.75" aria-hidden="true"></b-icon>
+                                                    <b-tooltip target="divisionExpoRestriction"
+                                                        title="Esta condición siempre y cuando a sea diferente de 0">
+                                                    </b-tooltip>
+                                                    <span>Para a ≠0</span>
+                                                </b-col>
+                                            </b-row>
+                                        </b-col>
+                                        <b-col cols="12" md="8" class="mt-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p style="text-align: left;">Ejemplo</p>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="sameBaseLessExpo1" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Exponente m : {{sameBaseLessExpo1}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="sameBaseLessExpo2" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Exponente n : {{sameBaseLessExpo2}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col>
+                                                    <span>
+                                                        <span class="description">a</span>
+                                                        <sup class="expo">{{sameBaseLessExpo1}}</sup>
+                                                        <span> / </span>
+                                                        <span class="description">a</span>
+                                                        <sup class="expo">{{sameBaseLessExpo2}}</sup>
+
+                                                    </span>
+                                                    <span> = </span>
+                                                    <span>
+                                                        <span class="description">a</span>
+                                                        <sup id="divisionExpoSolution"
+                                                            class="expo">{{sameBaseLessExpo1}}
+                                                            - {{sameBaseLessExpo2}}</sup>
+                                                    </span>
+                                                    <b-tooltip target="divisionExpoSolution"
+                                                        :title="divisionExpoSolutionDesc" />
+                                                    <span> = </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">{{sameBaseLessExpo1 - sameBaseLessExpo2}}</sup>
                                                 </b-col>
                                             </b-row>
                                         </b-col>
@@ -92,8 +196,18 @@
         name: 'ExponentPower',
         data() {
             return {
-                sameBaseMultiExpo1: 2,
-                sameBaseMultiExpo2: 3
+                sameBaseSumExpo1: 2,
+                sameBaseSumExpo2: 3,
+                sameBaseLessExpo1: 8,
+                sameBaseLessExpo2: 4
+            }
+        },
+        computed: {
+            multiExpoSolutionDesc() {
+                return `Sumamos los exponentes ${this.sameBaseSumExpo1} y ${this.sameBaseSumExpo2}`;
+            },
+            divisionExpoSolutionDesc() {
+                return `Restamos los exponentes ${this.sameBaseLessExpo1} y ${this.sameBaseLessExpo2}`;
             }
         },
     }
