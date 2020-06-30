672f5b6fe8f70426bdf1cfb5e6c9df3d4523b4f7
Roy <roy.rojval@gmail.com>
2020-06-30 16:34:38 -0500

    agregando multiplicacion

--- src/views/themes/algebra/exponentPower.vue
+++ src/views/themes/algebra/exponentPower.vue
@@ -186,6 +186,94 @@
                             </b-card-body>
                         </b-collapse>
                     </b-card>
+
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" role="tab">
+                            <b-button block v-b-toggle.accordion-3 variant="info">Potencia de una potencia
+                            </b-button>
+                        </b-card-header>
+                        <b-collapse id="accordion-3" visible accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text>
+                                    <b-row>
+                                        <b-col cols="12" md="4" class="mb-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p>Cuando tienen bases iguales, los exponentes se restan</p>
+                                                    <span> ( </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">m</sup>
+                                                    <span> ) </span>
+                                                    <sup class="expo">n</sup>
+                                                    <span> = </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">m x n</sup>
+                                                </b-col>
+                                            </b-row>
+
+                                        </b-col>
+                                        <b-col cols="12" md="8" class="mt-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p style="text-align: left;">Ejemplo</p>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="sameBaseMultiExpo1" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Exponente m : {{sameBaseMultiExpo1}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="sameBaseMultiExpo2" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Exponente n : {{sameBaseMultiExpo2}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col>
+                                                    <span>
+                                                        <span> ( </span>
+                                                        <span class="description">a</span>
+                                                        <sup class="expo">{{sameBaseMultiExpo1}}</sup>
+                                                        <span> ) </span>
+                                                        <sup class="expo">{{sameBaseMultiExpo2}}</sup>
+
+                                                    </span>
+                                                    <span> = </span>
+                                                    <span>
+                                                        <span class="description">a</span>
+                                                        <sup id="divisionExpoSolution"
+                                                            class="expo">{{sameBaseMultiExpo1}} x
+                                                            {{sameBaseMultiExpo2}}</sup>
+                                                    </span>
+                                                    <b-tooltip target="divisionExpoSolution"
+                                                        :title="divisionExpoSolutionDesc" />
+                                                    <span> = </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">{{sameBaseMultiExpo1 * sameBaseMultiExpo2}}</sup>
+                                                </b-col>
+                                            </b-row>
+                                        </b-col>
+                                    </b-row>
+                                </b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
                 </div>
             </b-col>
         </b-row>
@@ -199,7 +287,9 @@
                 sameBaseSumExpo1: 2,
                 sameBaseSumExpo2: 3,
                 sameBaseLessExpo1: 8,
-                sameBaseLessExpo2: 4
+                sameBaseLessExpo2: 4,
+                sameBaseMultiExpo1: 3,
+                sameBaseMultiExpo2: 4
             }
         },
         computed: {
