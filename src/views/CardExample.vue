ce77a399d8ee4598332a2563d6ac31e96438aabe
Roy <roy.rojval@gmail.com>
2020-06-30 19:41:04 -0500

    fix names

---
+++ src/views/CardExample.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-card no-body class="mb-1">
+            <b-card-header header-tag="header" role="tab">
+                <b-button block v-b-toggle.accordion variant="info">Ejemplo
+                </b-button>
+            </b-card-header>
+            <b-collapse id="accordion" visible accordion="my-accordion" role="tabpanel">
+                <b-card-body>
+                    <b-card-text>
+                        <span>AAA</span>
+                    </b-card-text>
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
--- src/views/themes/algebra/exponentPower.vue
+++ src/views/themes/algebra/exponentPower.vue
@@ -73,11 +73,10 @@
                                                     <span> = </span>
                                                     <span>
                                                         <span class="description">a</span>
-                                                        <sup id="multiExpoSolution" class="expo">{{sameBaseSumExpo1}}
+                                                        <sup id="sumExpoSolution" class="expo">{{sameBaseSumExpo1}}
                                                             + {{sameBaseSumExpo2}}</sup>
                                                     </span>
-                                                    <b-tooltip target="multiExpoSolution"
-                                                        :title="multiExpoSolutionDesc" />
+                                                    <b-tooltip target="sumExpoSolution" :title="sumExpoSolutionDesc" />
                                                     <span> = </span>
                                                     <span class="description">a</span>
                                                     <sup class="expo">{{sameBaseSumExpo1 + sameBaseSumExpo2}}</sup>
@@ -174,7 +173,7 @@
                                                             - {{sameBaseLessExpo2}}</sup>
                                                     </span>
                                                     <b-tooltip target="divisionExpoSolution"
-                                                        :title="divisionExpoSolutionDesc" />
+                                                        :title="lessExpoSolutionDesc" />
                                                     <span> = </span>
                                                     <span class="description">a</span>
                                                     <sup class="expo">{{sameBaseLessExpo1 - sameBaseLessExpo2}}</sup>
@@ -186,7 +185,6 @@
                             </b-card-body>
                         </b-collapse>
                     </b-card>
-
                     <b-card no-body class="mb-1">
                         <b-card-header header-tag="header" role="tab">
                             <b-button block v-b-toggle.accordion-3 variant="info">Potencia de una potencia
@@ -257,15 +255,119 @@
                                                     <span> = </span>
                                                     <span>
                                                         <span class="description">a</span>
-                                                        <sup id="divisionExpoSolution"
-                                                            class="expo">{{sameBaseMultiExpo1}} x
+                                                        <sup id="multiExpoSolution" class="expo">{{sameBaseMultiExpo1}}
+                                                            x
                                                             {{sameBaseMultiExpo2}}</sup>
                                                     </span>
-                                                    <b-tooltip target="divisionExpoSolution"
-                                                        :title="divisionExpoSolutionDesc" />
+                                                    <b-tooltip target="multiExpoSolution"
+                                                        :title="multiExpoSolutionDesc" />
                                                     <span> = </span>
                                                     <span class="description">a</span>
                                                     <sup class="expo">{{sameBaseMultiExpo1 * sameBaseMultiExpo2}}</sup>
+                                                </b-col>
+                                            </b-row>
+                                        </b-col>
+                                    </b-row>
+                                </b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
+
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" role="tab">
+                            <b-button block v-b-toggle.accordion-4 variant="info">Potencia de un Producto
+                            </b-button>
+                        </b-card-header>
+                        <b-collapse id="accordion-4" visible accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text>
+                                    <b-row>
+                                        <b-col cols="12" md="4" class="mb-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p>Cuando tienen un producto y un exponente los afecta</p>
+                                                    <span> ( </span>
+                                                    <span class="description">a</span>
+                                                    <span class="">x</span>
+                                                    <span class="description">b</span>
+                                                    <span> ) </span>
+                                                    <sup class="expo">m</sup>
+                                                    <span> = </span>
+                                                    <span class="description">a</span>
+                                                    <sup class="expo">m</sup>
+                                                    <span class="">x</span>
+                                                    <span class="description">b</span>
+                                                    <sup class="expo">m</sup>
+                                                </b-col>
+                                            </b-row>
+
+                                        </b-col>
+                                        <b-col cols="12" md="8" class="mt-2">
+                                            <b-row>
+                                                <b-col>
+                                                    <p style="text-align: left;">Ejemplo</p>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="comunBasefactor1" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Factor a : {{comunBasefactor1}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                                <b-col cols="12" md="6">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="comunBasefactor2" :max="50" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Factor b : {{comunBasefactor2}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col cols="12">
+                                                    <b-row>
+                                                        <b-col>
+                                                            <vue-slider v-model="sameBaseComunExpo" :max="10" />
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row class="mb-4">
+                                                        <b-col>
+                                                            <span>Exponente : {{sameBaseComunExpo}}</span>
+                                                        </b-col>
+                                                    </b-row>
+                                                </b-col>
+                                            </b-row>
+                                            <b-row>
+                                                <b-col>
+                                                    <span>
+                                                        <span> ( </span>
+                                                        <span class="description">{{comunBasefactor1}}</span>
+                                                        <span class="">x</span>
+                                                        <span class="description">{{comunBasefactor2}}</span>
+                                                        <span> ) </span>
+                                                        <sup class="expo">{{sameBaseComunExpo}}</sup>
+
+                                                    </span>
+                                                    <span> = </span>
+                                                    <span>
+                                                        <span class="description">{{comunBasefactor1}}</span>
+                                                        <sup class="expo">{{sameBaseComunExpo}}</sup>
+                                                        <span class="">x</span>
+
+                                                        <span class="description">{{comunBasefactor2}}</span>
+                                                        <sup class="expo">{{sameBaseComunExpo}}</sup>
+                                                    </span>
                                                 </b-col>
                                             </b-row>
                                         </b-col>
@@ -289,15 +391,21 @@
                 sameBaseLessExpo1: 8,
                 sameBaseLessExpo2: 4,
                 sameBaseMultiExpo1: 3,
-                sameBaseMultiExpo2: 4
+                sameBaseMultiExpo2: 4,
+                sameBaseComunExpo: 5,
+                comunBasefactor1: 2,
+                comunBasefactor2: 3
             }
         },
         computed: {
-            multiExpoSolutionDesc() {
+            sumExpoSolutionDesc() {
                 return `Sumamos los exponentes ${this.sameBaseSumExpo1} y ${this.sameBaseSumExpo2}`;
             },
-            divisionExpoSolutionDesc() {
+            lessExpoSolutionDesc() {
                 return `Restamos los exponentes ${this.sameBaseLessExpo1} y ${this.sameBaseLessExpo2}`;
+            },
+            multiExpoSolutionDesc() {
+                return `Multiplicamos los exponentes ${this.sameBaseMultiExpo1} y ${this.sameBaseMultiExpo2}`;
             }
         },
     }
