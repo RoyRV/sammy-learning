d3a88364931ad111fcbf6a48f71d2ab28f41984b
Roy <roy.rojval@gmail.com>
2021-04-14 12:41:14 -0500

    update

---
+++ src/components/average/armonicAverage.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <b-row>
+            <b-col cols="12">
+                <p style="text-align: left;">El promedio armónico o media armónica se obtiene al dividir el número de
+                    datos entre la suma de inversas de los datos</p>
+            </b-col>
+            <b-col cols="12">
+                <p style="text-align: left;">Dados los números:</p>
+                <p>
+                    <span class="description">a</span><em><sub>1</sub></em>
+                    <span class="description"> , a </span><em><sub>2</sub></em>
+                    <span class="description"> , a </span><em><sub>3</sub></em>
+                    <span class="description"> , a </span><em><sub>4</sub></em>
+                    <span class="description"> ... </span>
+                    <span class="description"> , a </span><em><sub>n-2</sub></em>
+                    <span class="description"> , a </span><em><sub>n-1</sub></em>
+                    <span class="description"> , a </span><em><sub>n</sub></em>
+                </p>
+            </b-col>
+            <b-col cols="12">
+                <b-row>
+                    <b-col cols="12">
+                        <p style="text-align: left;">La media armonica se calcula :</p>
+                    </b-col>
+                    <b-col cols="12">
+                        <b-row>
+                            <b-col sm="4" md="4" lg="4" class="mb-3">
+                                <span class="description">M.H. = </span>
+                            </b-col>
+                            <b-col sm="8" md="8" lg="8">
+                                <b-row>
+                                    <b-col cols="12">
+                                        <b-row>
+                                            <b-col>
+                                                <span>cantidad de datos</span>
+                                            </b-col>
+                                        </b-row>
+                                        <b-row>
+                                            <b-col>
+                                                <span>————————————————————</span>
+                                            </b-col>
+                                        </b-row>
+                                        <b-row>
+                                            <b-col>
+                                                <span>Sumatoria de las inversas de los números</span>
+                                            </b-col>
+                                        </b-row>
+                                    </b-col>
+                                </b-row>
+                            </b-col>
+                        </b-row>
+                    </b-col>
+
+                    <b-col cols="12">
+                        <b-row>
+                            <b-col sm="4" md="4" lg="4" class="mb-3">
+                                <span class="description">M.H. = </span>
+                            </b-col>
+                            <b-col sm="8" md="8" lg="8">
+                                <b-row>
+                                    <b-col cols="12">
+                                        <b-row>
+                                            <b-col>
+                                                <span>n</span>
+                                            </b-col>
+                                        </b-row>
+                                        <b-row>
+                                            <b-col>
+                                                <span>————————————————————</span>
+                                            </b-col>
+                                        </b-row>
+                                        <b-row>
+                                            <b-col>
+                                                <span class="">1/a</span><em><sub>1</sub></em>
+                                                <span class=""> + 1/a </span><em><sub>2</sub></em>
+                                                <span class=""> +.. </span>
+                                                <span class=""> + 1/a </span><em><sub>n-1</sub></em>
+                                                <span class=""> + 1/a </span><em><sub>n</sub></em>
+                                            </b-col>
+                                        </b-row>
+                                    </b-col>
+                                </b-row>
+                            </b-col>
+                        </b-row>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ArmonicAveragenComponent',
+    }
+</script>
+<style scoped>
+    .description {
+        font-family: "MathJax_Math-italic";
+        font-style: normal;
+        font-weight: normal;
+        line-height: normal;
+        font-size: 35px;
+    }
+</style>
--- src/views/themes/arithmetic/averages.vue
+++ src/views/themes/arithmetic/averages.vue
@@ -32,6 +32,18 @@
                             </b-card-body>
                         </b-collapse>
                     </b-card>
+                    <b-card no-body class="mb-1">
+                        <b-card-header header-tag="header" role="tab">
+                            <b-button block v-b-toggle.accordion-3 variant="info">Promedio Armonico / Media Armonica (M.H.)</b-button>
+                        </b-card-header>
+                        <b-collapse id="accordion-3" visible accordion="my-accordion" role="tabpanel">
+                            <b-card-body>
+                                <b-card-text>
+                                   <ArmonicAverage></ArmonicAverage>
+                                </b-card-text>
+                            </b-card-body>
+                        </b-collapse>
+                    </b-card>
                 </div>
             </b-col>
         </b-row>
@@ -40,8 +52,9 @@
 <script>
     import ArithmeticAverage from '@/components/average/arithmeticAverage.vue';
     import GeometricAverage from '@/components/average/geometricAverage.vue';
+    import ArmonicAverage from '@/components/average/armonicAverage.vue';
      export default {
         name: 'Average',
-        components : {ArithmeticAverage , GeometricAverage}
+        components : {ArithmeticAverage , GeometricAverage , ArmonicAverage}
      }
 </script>
